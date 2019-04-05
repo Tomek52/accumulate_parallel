858c352872af2cac00c7b1bae3fd3395f4327abd
tomek <tomek@debian.tomek>
2019-04-05 18:43:14 +0200

    fix race condition, improve performance when numOfElements = 0

--- accumulate.cpp
+++ accumulate.cpp
@@ -9,20 +9,22 @@ template<class InputIt, class T>
 T accumulateParallel(InputIt first, InputIt last, T init, int numOfThreads)
 {
     int numOfElements = std::distance(first,last);
+    if(numOfElements==0) return init;
     int sizeOfPart = ceil(static_cast<double>(numOfElements)/static_cast<double>(numOfThreads));
     std::vector<std::thread> threadsSum;
+    std::vector<T> partialSum;
     for(int i = 0; i<numOfThreads; i++) 
     {
-        threadsSum.emplace_back([=, &init](){init+=std::accumulate(first,first+sizeOfPart,0);});
+        threadsSum.emplace_back([=, &partialSum](){partialSum.emplace_back(std::accumulate(first,first+sizeOfPart,0));});
         first+=sizeOfPart;
     }
     for(auto && thread: threadsSum) thread.join();
-    return init;
+    return std::accumulate(partialSum.begin(),partialSum.end(),0);
 }
 
 int main()
 {
-    std::vector<int> v(4450001,2);
+    std::vector<double> v(4450001,2);
     auto x = accumulateParallel(v.begin(), v.end(), 0, 4);
     if(std::accumulate(v.begin(),v.end(),0)==x)
     {
